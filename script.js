f875dad4b36bde67b5b53c45b25c8a6d9d61bd49
Rainbowcats999 <526erizov@frhsd.com>
2025-04-28 09:26:50 -0400

    Yayyyy I made the text type one by one on each page

--- script.js
+++ script.js
@@ -13,6 +13,20 @@ function pauseAudio() {
 // Create a new Audio object for the hiss sound
 const hiss = new Audio("hiss.mp3");
 
+window.onload = function () {
+  console.log("here");
+  type(
+    `In season one of Cobra Kai, thirty years after their rivalry, Johnny Lawrence
+            and Daniel
+            LaRusso are reintroduced as adults with conflicting paths. Johnny, struggling with his past, reopens the
+            Cobra Kai dojo, while Daniel runs a successful business. Johnny's Cobra Kai students are initially bullied
+            outcasts, but they eventually become arrogant and aggressive, embracing the "No Mercy" mantra. The season
+            culminates in the All Valley Under-18 Karate Tournament, where Johnny and Daniel's students compete,
+            reigniting their rivalry.`,
+    document.getElementById("s1")
+  );
+};
+
 // Check if the audio file loads correctly
 hiss.onerror = () => {
   console.error("Error loading the hiss sound!");
@@ -29,3 +43,16 @@ cobraImg.addEventListener("mouseenter", () => {
     console.error("Error playing the sound:", error);
   });
 });
+
+function type(arc, cont) {
+  let string = arc.split("");
+  for (let i = 0; i < string.length; i++) {
+    setTimeout(
+      () => {
+        cont.append(string[i]);
+      },
+      i * 50,
+      string[i]
+    );
+  }
+}
